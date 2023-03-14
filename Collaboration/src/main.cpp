496dfeb195837350b4edd1f09d0c9ffe38402818
Jython1415 <127829015+rh0820@users.noreply.github.com>
2023-03-14 17:34:13 +0900

    Auto
    auto

--- Collaboration/src/main.cpp
+++ Collaboration/src/main.cpp
@@ -14,8 +14,48 @@
 
 using namespace vex;
 
-int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
+controller ctrl;
+motor f_Right(PORT10,true);
+motor b_Right(PORT20,true);
+motor f_Left(PORT12);
+motor b_Left(PORT3);
+motor_group c_left (f_Left,b_Left);
+motor_group c_right (f_Right,b_Right);
+competition cmp;
+
+
+
+
+
+void auton() {
+   c_left.spin(directionType::fwd, 100, velocityUnits::pct);
+   c_right.spin(directionType::fwd, 100, velocityUnits::pct);
+   task::sleep(5000);
+
+
+   c_left.spin(directionType::fwd, 100, velocityUnits::pct);
+   c_right.spin(directionType::fwd, -100, velocityUnits::pct);
+   task::sleep(3000);
+
+
+   c_left.stop();
+   c_right.stop();
+}
+
+
+
+
+
+int main(){
+
   vexcodeInit();
-  
+  cmp.autonomous(auton);
+
+
+
+
+
+
+
+
 }
